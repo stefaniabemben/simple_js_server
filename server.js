dd0204049c2a563c8dc2380ffed9a3c6102af605
stefaniabemben <137799832+stefaniabemben@users.noreply.github.com>
2023-06-28 19:17:16 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Grupa ZDTESTpol125 jest super SUPER!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(port);
