14d5928ad74481e71d625c1d0acaee1312cb8575
stefaniabemben <137799832+stefaniabemben@users.noreply.github.com>
2023-06-28 19:31:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Grupa ZDTESTpol125 jest super SUPER!');
+  res.end('Stefania jest super SUPER!');
 }
 
 const server = http.createServer(requestListener);
